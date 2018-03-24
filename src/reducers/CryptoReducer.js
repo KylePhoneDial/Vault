5ea2d9b32509779d34234df9aa652b0c545bce5d
KylePhoneDial <KylePhoneDial@gmail.com>
2018-03-23 22:28:03 -0500

    Setup CryptoReducer

--- src/reducers/CryptoReducer.js
+++ src/reducers/CryptoReducer.js
@@ -1,3 +1,43 @@
+import {
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_FAIL,
+  FETCHING_COIN_SUCCESS
+} from "./..utils/ActionTypes";
+
+const initialState = {
+  isFetching: null,
+  data: [],
+  hasError: false,
+  errorMessage: null
+};
+
 export default function(state = [], action) {
-    return state;
+  switch (action.type) {
+    case FETCHING_COIN_DATA:
+      return Object.assign({}, state, {
+        isFetching: true,
+        data: null,
+        haseError: false,
+        errorMessage: null
+      });
+
+    case FETCHING_COIN_DATA_FAIL:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        haseError: true,
+        errorMessage: action.err
+      });
+
+    case FETCHING_COIN_DATA:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        haseError: false,
+        errorMessage: null
+      });
+
+    default:
+      return state;
+  }
 }
