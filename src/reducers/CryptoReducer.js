6c93037ae2967b32f84d8eb246fc34b0b3886a7f
KylePhoneDial <KylePhoneDial@gmail.com>
2018-03-23 22:30:09 -0500

    Fixed error with importing action types

--- src/reducers/CryptoReducer.js
+++ src/reducers/CryptoReducer.js
@@ -2,7 +2,7 @@ import {
   FETCHING_COIN_DATA,
   FETCHING_COIN_FAIL,
   FETCHING_COIN_SUCCESS
-} from "./..utils/ActionTypes";
+} from "./../utils/ActionTypes";
 
 const initialState = {
   isFetching: null,
@@ -12,32 +12,33 @@ const initialState = {
 };
 
 export default function(state = [], action) {
-  switch (action.type) {
-    case FETCHING_COIN_DATA:
-      return Object.assign({}, state, {
-        isFetching: true,
-        data: null,
-        haseError: false,
-        errorMessage: null
-      });
+  
+    switch(action.type) {
+        case FETCHING_COIN_DATA:
+            return Object.assign({}, state, {
+                isFetching: true,
+                data: null,
+                haseError: false,
+                errorMessage: null
+            });
 
-    case FETCHING_COIN_DATA_FAIL:
-      return Object.assign({}, state, {
-        isFetching: false,
-        data: action.payload,
-        haseError: true,
-        errorMessage: action.err
-      });
+        case FETCHING_COIN_DATA_FAIL:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                haseError: true,
+                errorMessage: action.err
+            });
 
-    case FETCHING_COIN_DATA:
-      return Object.assign({}, state, {
-        isFetching: false,
-        data: action.payload,
-        haseError: false,
-        errorMessage: null
-      });
+        case FETCHING_COIN_DATA:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                haseError: false,
+                errorMessage: null
+            });
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 }
